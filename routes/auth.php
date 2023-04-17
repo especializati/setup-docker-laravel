38a314be0b1d48cb72cbf3b2ea44642aa06d2de4
Carlos Ferreira <carlos@especializati.com.br>
2023-04-17 17:03:21 -0300

    fix: change url reset password (adminlte)

--- routes/auth.php
+++ routes/auth.php
@@ -22,10 +22,10 @@ Route::middleware('guest')->group(function () {
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+    Route::get('password/reset', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
 
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+    Route::post('password/reset', [PasswordResetLinkController::class, 'store'])
                 ->name('password.email');
 
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
